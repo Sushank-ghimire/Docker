51b41f4d096c4513e0f51277f51033d0d231c21a
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-08-15 19:37:05 +0545

    Updated packages and added dotenv package in dependencies

--- nodejs/app/index.ts
+++ nodejs/app/index.ts
@@ -1,14 +1,18 @@
 import express, { Request, Response } from "express";
 import http from "node:http";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 const server = http.createServer(app);
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
+  const secret = process.env.SECRET_KEY || "key not found";
   res.json({ message: "Server is running on docker container" });
 });
 
